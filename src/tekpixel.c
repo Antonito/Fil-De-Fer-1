8891b8efd638696b48ec851ec09d79697a5179fb
Antoine Baché <antoine.bache@epitech.eu>
2015-11-21 19:54:16 +0100

    Fix compil

--- src/tekpixel.c
+++ src/tekpixel.c
@@ -5,13 +5,13 @@
 ** Login   <bache_a@epitech.net>
 ** 
 ** Started on  Sat Nov 14 13:42:40 2015 Antoine Baché
-** Last update Sat Nov 21 19:52:17 2015 Antoine Baché
+** Last update Sat Nov 21 19:54:09 2015 Antoine Baché
 */
 
 #include "../include/my.h"
 
 void	tekpixel(t_bunny_pixelarray *pix,
-		 xt_bunny_position *pos, t_color *color)
+		 t_bunny_position *pos, t_color *color)
 {
   t_color	*tmp;
 
